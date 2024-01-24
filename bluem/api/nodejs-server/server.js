9763b32938f17fe5bee50aaa84e80594e3d07720
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2024-01-24 17:36:55 +0100

    yarn server prep completed

---
+++ bluem/api/nodejs-server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.use(require('cors')())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./src/router'));
+
+const { createConnection } = require('./src/database');
+
+createConnection()
+    .then(() => {
+        app.listen(port, () => {
+
+            console.log(`App is listening on port: ${port}`);
+
+        });
+    })
+    .catch(console.log)
